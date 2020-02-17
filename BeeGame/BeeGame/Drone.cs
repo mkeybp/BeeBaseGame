6430479a49ee0bd63b4f72d5fd60b9e001d65bd6
Lyhne1997 <55769759+Lyhne1997@users.noreply.github.com>
2020-02-17 11:01:16 +0100

    123

--- BeeGame/BeeGame/Drone.cs
+++ BeeGame/BeeGame/Drone.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace BeeGame
 {
-    class Drone
+    class Drone : GameObject
     {
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
